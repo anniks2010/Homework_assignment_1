1559c30c79d1b315ddc32ba22f0e84c327a09ddd
Anu <anniks_4@hotmail.com>
2019-09-20 20:40:53 +0300

    rounding is done

--- Homework_assignment/Program.cs
+++ Homework_assignment/Program.cs
@@ -15,66 +15,60 @@ namespace Homework_assignment
             float pikkus;
             int kaal;
             float kehaMass;
-
+            
             Console.WriteLine("What is your name?");
             name = Console.ReadLine();
             Console.WriteLine("Nice to meet you, {0}", name);
             Console.WriteLine("How old are you?");
             age = int.Parse(Console.ReadLine());
 
-            if (age < 20 || age > 65)
+            if (age < 20)
             {
-                Console.WriteLine("You are to young or old for that calculations");
+                Console.WriteLine("You are too young for that calculations.");
                 Console.ReadLine();
             }
-
+            else if (age > 65)
+            {
+                Console.WriteLine("You are too old for that calculations.");
+                Console.ReadLine();
+            }
             else
-
             {
                 Console.WriteLine("How tall are you in meters?");
                 pikkus = float.Parse(Console.ReadLine());
-
                 Console.WriteLine("What is your weight (kg)?");
                 kaal = int.Parse(Console.ReadLine());
-                kehaMass = kaal / (pikkus * pikkus);
+                kehaMass = (float)(Math.Round((double)(kaal / (pikkus * pikkus)), 2));
 
                 if (kehaMass < 16) 
                 {
-                    Console.WriteLine("{0}, your kehamassiindeks is {1}. It is ohtlik sinu alakaal", name, kehaMass);
+                    Console.WriteLine($"{name}, your kehamassiindeks is {kehaMass}. It is ohtlik sinu alakaal.");                    
                 }
-
                 else if (kehaMass >= 16 && kehaMass <= 18.5)
-
                 {
-                    Console.WriteLine("{0}, your kehamassiindeks is {1}. It is alakaal", name, kehaMass);
+                    Console.WriteLine($"{name}, your kehamassiindeks is {kehaMass}. It is alakaal.");
                 }
-
                 else if (kehaMass >= 18.6 && kehaMass <= 25)
                 {
-                    Console.WriteLine("{0}, your kehamassiindeks is {1}. It is normaalkaal", name, kehaMass);
+                    Console.WriteLine($"{name}, your kehamassiindeks is {kehaMass}. It is normaalkaal.");
                 }
-
                 else if (kehaMass >= 25.1 && kehaMass <= 30)
                 {
-                    Console.WriteLine("{0}, your kehamassiindeks is {1}. It is ülekaal", name, kehaMass);
+                    Console.WriteLine($"{name}, your kehamassiindeks is {kehaMass}. It is ülekaal.");
                 }
-
                 else if (kehaMass >= 30.1 && kehaMass <= 35)
                 {
-                    Console.WriteLine("{0}, your kehamassiindeks is {1}. It is rasvumine", name, kehaMass);
+                    Console.WriteLine($"{name}, your kehamassiindeks is {kehaMass}. It is rasvumine.");
                 }
                 else if (kehaMass >= 35.1 && kehaMass <= 40)
                 {
-                    Console.WriteLine("{0}, your kehamassiindeks is {1}. It is tugev rasvumine", name, kehaMass);
+                    Console.WriteLine($"{name}, your kehamassiindeks is {kehaMass}. It is tugev rasvumine.");
                 }
                 else 
                 {
-                    Console.WriteLine("{0}, your kehamassiindeks is {1}. It is tervisele ohtlik rasvumine", name, kehaMass);
+                    Console.WriteLine($"{name}, your kehamassiindeks is {kehaMass}. It is tervisele ohtlik rasvumine.");
                 }
-
-                
                 Console.ReadLine();
-
             }
         }
     }
